75cf1fa799d7a59057ca07affd562aabbf868a05
Edson.Souza <edson.souza@intelitrader.com.br>
2021-03-30 22:49:47 -0300

    Init repository

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleChat.WebApp.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Hello World!";
+            return View();
+        }
+
+    }
+}
